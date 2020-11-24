3f3fcc007e2abd1e8963b1ea5205be88eccfccd1
SumeetDevkai <47910905+SumeetDevkai@users.noreply.github.com>
2020-11-24 15:03:32 +0530

    Added the stickman project to this repository

---
+++ StickmanDM/Assets/Scripts/Photon/GameSetupController.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameSetupController : MonoBehaviour
+{
+
+    List<GameObject> players;
+
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreatePlayer()
+    {
+        GameObject x = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), new Vector3(0,1,0), Quaternion.identity); // "PhotonPlayer = player gameobject name"
+  
+    }
+
+}
